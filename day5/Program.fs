0edd915385f196ccb3ea27c5ec97eca364537c37
Johan Sandred <johan.sandred@protonmail.com>
2022-12-06 20:59:35 +0100

    Add day5 and day6

---
+++ day5/Program.fs
@@ -0,0 +1,138 @@
+﻿let input =
+    System.IO.File.ReadLines("input.txt")
+    |> Seq.toList
+
+let testinput =
+    [ "    [D]    "
+      "[N] [C]    "
+      "[Z] [M] [P]"
+      " 1   2   3 "
+      ""
+      "move 1 from 2 to 1"
+      "move 3 from 1 to 3"
+      "move 2 from 2 to 1"
+      "move 1 from 1 to 2" ]
+
+let rec splitOnEmptyStringHelper part1 part2 =
+    match part2 with
+    | "" :: xs -> List.rev part1, xs
+    | x :: xs -> splitOnEmptyStringHelper (x :: part1) xs
+    | _ -> failwith "bad input"
+
+let splitOnEmptyString = splitOnEmptyStringHelper []
+
+let initstate (strings: list<string>) =
+    strings
+    |> Seq.map (fun str -> str.ToCharArray() |> Array.chunkBySize 4)
+
+let createStacks (rows: char [] [] list) : char list [] =
+    rows
+    |> List.map (Seq.map (Array.item 1))
+    |> function
+        | x :: xs ->
+            xs
+            |> List.fold
+                (fun columns crates ->
+                    columns
+                    |> Seq.zip crates
+                    |> Seq.map (fun (crate, column) ->
+                        if crate <> ' ' then
+                            crate :: column
+                        else
+                            column))
+                (x
+                 |> Seq.map (fun elem -> [ if elem <> ' ' then elem ]))
+            |> Seq.map List.tail
+            |> Seq.map List.rev
+            |> Seq.toArray
+        | _ -> failwith "bad input"
+
+let toString: char list -> string = List.map string >> String.concat ""
+
+let createMoves (moveStrings: list<string>) : seq<int * int * int> =
+    moveStrings
+    |> Seq.map (fun line ->
+        line
+        // FIXME: numbers can have multiple digits
+        |> Seq.fold
+            (fun state c ->
+                let strings, currentNumberString = state
+
+                if c >= '0' && c <= '9' then
+                    strings, (c :: currentNumberString)
+                else if List.isEmpty currentNumberString then
+                    strings, currentNumberString
+                else
+                    ((List.rev currentNumberString) :: strings), [])
+            ([], [])
+        |> function
+            | [ b; a ], c -> a |> toString |> int, b |> toString |> int, c |> toString |> int
+            | _ -> failwith "badinput")
+
+let part1 stateInput movesInput =
+
+    let rows = initstate stateInput |> Seq.toList
+    let moves = createMoves movesInput
+    let stacks = createStacks rows
+
+    moves
+    |> Seq.fold
+        (fun (currentStacks: list<char> []) (crateCount, fromColumnIndex, toColumnIndex) ->
+            let fromColumn = currentStacks[fromColumnIndex - 1]
+            let toColumn = currentStacks[toColumnIndex - 1]
+            let cratesToMove, newFromColumn = fromColumn |> List.splitAt crateCount
+            currentStacks[fromColumnIndex - 1] <- newFromColumn
+
+            currentStacks[toColumnIndex - 1] <- List.concat [ cratesToMove |> List.rev
+                                                              toColumn ]
+
+            currentStacks)
+        stacks
+    |> Seq.map Seq.head
+    |> Seq.map string
+    |> String.concat ""
+
+let part2 stateInput movesInput =
+    let rows = initstate stateInput |> Seq.toList
+    let moves = createMoves movesInput
+    let stacks = createStacks rows
+
+    moves
+    |> Seq.fold
+        (fun (currentStacks: list<char> []) (crateCount, fromColumnIndex, toColumnIndex) ->
+            let fromColumn = currentStacks[fromColumnIndex - 1]
+            let toColumn = currentStacks[toColumnIndex - 1]
+            let cratesToMove, newFromColumn = fromColumn |> List.splitAt crateCount
+            currentStacks[fromColumnIndex - 1] <- newFromColumn
+
+            currentStacks[toColumnIndex - 1] <- List.concat [ cratesToMove; toColumn ]
+
+            currentStacks)
+        stacks
+    |> Seq.map Seq.head
+    |> Seq.map string
+    |> String.concat ""
+
+
+
+
+let teststateInput, testmovesInput = splitOnEmptyString testinput
+let stateInput, movesInput = splitOnEmptyString input
+
+let testpart1Answer = "CMZ"
+let testpart1Result = part1 teststateInput testmovesInput
+assert (testpart1Result = testpart1Answer)
+
+let part1Result = part1 stateInput movesInput
+let part1Answer = "VRWBSFZWM"
+printfn "%s" part1Result
+assert (part1Result = part1Answer)
+
+let testpart2Answer = "MCD"
+let testpart2Result = part2 teststateInput testmovesInput
+assert (testpart2Result = testpart2Answer)
+
+let part2Result = part2 stateInput movesInput
+let part2Answer = "RBTWJWMCF"
+printfn "%s" part2Result
+assert (part2Result = part2Answer)
---
+++ day6/Program.fs
@@ -0,0 +1,61 @@
+﻿let input = System.IO.File.ReadAllText("input.txt")
+
+let testinputs =
+    [ "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+      "bvwbjplbgvbhsrlpgdmjqwftvncz"
+      "nppdvjthqldpwncqszvftbrmjlhg"
+      "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+      "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" ]
+
+let test1Answers = [ 7; 5; 6; 10; 11 ]
+let test2Answers = [ 19; 23; 23; 29; 26 ]
+
+let tests1 = Seq.zip testinputs test1Answers
+let tests2 = Seq.zip testinputs test2Answers
+
+let part1 windowSize (input: string) =
+    let toIndex c = (c |> int) - ('a' |> int)
+    let alphabetSize = toIndex 'z' - toIndex 'a' + 1
+    let lookupTable = Array.replicate alphabetSize 0
+
+    input
+    |> Seq.take (windowSize - 1)
+    |> Seq.iter (fun c -> lookupTable[toIndex c] <- lookupTable[toIndex c] + 1)
+
+    let foo =
+        (input
+         |> Seq.windowed windowSize
+         |> Seq.map (fun window ->
+             let headIndex = toIndex (window[0])
+             let tailIndex = toIndex (window[windowSize - 1])
+
+             lookupTable[headIndex] <- lookupTable[headIndex] - 1
+             lookupTable[tailIndex] <- lookupTable[tailIndex] + 1
+             window)
+         |> Seq.findIndex (fun window ->
+             lookupTable[toIndex window[0]] = 0
+             && window
+                |> Seq.tail
+                |> Seq.forall (fun c -> lookupTable[toIndex c] = 1)))
+
+    foo + windowSize
+
+tests1
+|> Seq.iter (fun (input, answer) ->
+    let testresult = part1 4 input
+    assert (testresult = answer))
+
+let part1result = part1 4 input
+printfn "%d" part1result
+let part1Answer = 1658
+assert (part1result = part1Answer)
+
+tests2
+|> Seq.iter (fun (input, answer) ->
+    let testresult = part1 14 input
+    assert (testresult = answer))
+
+let part2result = part1 14 input
+printfn "%d" part2result
+let part2Answer = 2260
+assert (part2result = part2Answer)
